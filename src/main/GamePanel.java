46bc0973318496cd060dcefdb5536af190a5b2c6
suyog <mgrsuyog6@gmail.com>
2023-12-12 21:10:18 +0545

    new

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,15 @@
 package main;
 
-public class GamePanel {
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	public GamePanel() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,5 +1,15 @@
 package main;
+import javax.swing.JFrame;
 
 public class GameWindow {
-
+	private JFrame jFrame;
+	public GameWindow(){
+		jFrame = new JFrame();
+		
+		jFrame.setTitle("New Game");
+		jFrame.setSize(400,400);
+		jFrame.setLocationRelativeTo(null);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setVisible(true);
+	}
 }
