2814df11850e3ac104045f9cc0da4f394b041bb8
suyog <mgrsuyog6@gmail.com>
2023-12-13 11:56:54 +0545

    Inputs

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -3,13 +3,14 @@ import javax.swing.JFrame;
 
 public class GameWindow {
 	private JFrame jFrame;
-	public GameWindow(){
+	public GameWindow(GamePanel gamePanel){
 		jFrame = new JFrame();
 		
 		jFrame.setTitle("New Game");
 		jFrame.setSize(400,400);
 		jFrame.setLocationRelativeTo(null);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.add(gamePanel);
 		jFrame.setVisible(true);
 	}
 }
