a5dd06476f4ec993375773ae4119fb5a4317c0fe
suyog <mgrsuyog6@gmail.com>
2023-12-16 18:22:42 +0545

    Mouse and KeyBoard Inputs

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,8 +3,15 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class KeyboardInputs implements KeyListener{
+import main.GamePanel;
 
+public class KeyboardInputs implements KeyListener{
+	private GamePanel gamePanel;
+	
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -19,8 +26,21 @@ public class KeyboardInputs implements KeyListener{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		System.out.println("A key is Pressed");
+		switch(e.getKeyCode()) {		
 		
+		case KeyEvent.VK_W:
+			gamePanel.changeYDelta(-15);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.changeXDelta(-15);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.changeYDelta(15);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.changeXDelta(15);
+			break;
+		}
 	}
 	
 }
