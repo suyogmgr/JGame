d55a9a87d00fdb399bd85788b3c3205de330b7bb
suyog <mgrsuyog6@gmail.com>
2023-12-21 21:10:21 +0545

    m

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,11 +1,22 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
+	private Thread gameThread;
+	private final FPS_SET = 120.0;
+	
 	public Game() {
+		
 		gamePanel = new GamePanel();
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();
+	}
+	public void run() {
+		
+		while(true) {
+			
+		}
+		
 	}
 }
